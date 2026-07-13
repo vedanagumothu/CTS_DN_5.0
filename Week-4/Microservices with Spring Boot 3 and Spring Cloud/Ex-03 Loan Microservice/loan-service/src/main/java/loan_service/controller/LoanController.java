bbe924fde83c1978c20275c4a00d6c7e1ccbc14d
vedanagumothu <23501a4441@pvpsit.ac.in>
2026-07-13 22:37:21 +0530

    Add Week 4 Microservices exercises

---
+++ Week-4/Microservices with Spring Boot 3 and Spring Cloud/Ex-03 Loan Microservice/loan-service/src/main/java/loan_service/controller/LoanController.java
@@ -0,0 +1,14 @@
+package com.cognizant.loanservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoanController {
+
+    @GetMapping("/loan")
+    public String getLoan() {
+        return "Loan Service Running";
+    }
+
+}
